2aef7c4c589a4053ec4f02ab2115eb991006d554
Andrei <andrei@constantinescu.io>
2016-10-21 17:20:12 +0100

    Add qlmarkdown.

--- defaults/osx.sh
+++ defaults/osx.sh
@@ -8,7 +8,7 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until `.osx` has finished.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-echo "\nGlobal defaults."
+echo "\nGlobal."
 
 echo "- Expand save panel."
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
@@ -25,7 +25,7 @@ echo "- Use scroll gesture with the Ctrl (^) modifier key to zoom."
 defaults write com.apple.universalaccess closeViewScrollWheelToggle -bool true
 defaults write com.apple.universalaccess HIDScrollZoomModifierMask -int 262144
 
-echo "\nMenu bar defaults."
+echo "\nMenu bar."
 
 echo "- Customize the clock look."
 defaults write com.apple.menuextra.clock DateFormat -string "EEE d MMM  HH:mm:ss"
@@ -33,7 +33,7 @@ defaults write com.apple.menuextra.clock DateFormat -string "EEE d MMM  HH:mm:ss
 echo "- Change the battery to show the percentage."
 defaults write com.apple.menuextra.battery ShowPercent -string "YES"
 
-echo "\nKeyboard defaults."
+echo "\nKeyboard."
 
 echo "- Set Keyboard > Key Repeat to be the fastest possible from System Preferences."
 defaults write NSGlobalDomain KeyRepeat -integer 2
@@ -41,7 +41,7 @@ defaults write NSGlobalDomain KeyRepeat -integer 2
 echo "- Set Keyboard > Delay Until Repeat to be the fastest possible from System Preferences."
 defaults write NSGlobalDomain InitialKeyRepeat -integer 15
 
-echo "\nTrackpad defaults."
+echo "\nTrackpad."
 
 echo "- Enable three finger drag."
 defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag -integer 1
@@ -55,7 +55,7 @@ defaults write NSGlobalDomain com.apple.trackpad.threeFingerSwipeGesture -intege
 echo "- Enable four finger Exposé."
 defaults write com.apple.dock showAppExposeGestureEnabled -integer 1
 
-echo "\nDock defaults."
+echo "\nDock."
 
 echo "- Turn on dock autohiding."
 defaults write com.apple.Dock autohide -boolean true
@@ -66,7 +66,7 @@ defaults write com.apple.Dock tilesize -integer 48
 echo "- Wipe all (default) app icons from the Dock."
 defaults write com.apple.dock persistent-apps -array ""
 
-echo "\nFinder defaults."
+echo "\nFinder."
 
 echo "- Set default path to HOME directory."
 defaults write com.apple.finder NewWindowTarget -string "PfLo"
@@ -98,7 +98,7 @@ defaults write com.apple.finder ShowHardDrivesOnDesktop -bool false
 defaults write com.apple.finder ShowMountedServersOnDesktop -bool false
 defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool false
 
-echo "\nSafari defaults."
+echo "\nSafari."
 
 echo "- Set home page to about:blank."
 defaults write com.apple.Safari HomePage -string "about:blank"
@@ -117,13 +117,13 @@ defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
 
-echo "\nSpotlight defaults."
+echo "\nSpotlight."
 
 echo "- Disable Spotlight indexing for any volume that gets mounted and has not yet been indexed before."
 # Use `sudo mdutil -i off "/Volumes/foo"` to stop indexing any volume.
 sudo defaults write /.Spotlight-V100/VolumeConfiguration Exclusions -array "/Volumes"
 
-echo "\nTransmission.app defaults."
+echo "\nTransmission.app."
 
 echo "- Use ~/Downloads to store incomplete downloads, and as default download folder."
 defaults write org.m0k.transmission UseIncompleteDownloadFolder -bool true
@@ -141,5 +141,8 @@ defaults write org.m0k.transmission WarningDonate -bool false
 
 echo "- Hide the legal disclaimer."
 defaults write org.m0k.transmission WarningLegal -bool false
+
+echo "- Enable QlMarkdown selecting text."
+defaults write com.apple.finder QLEnableTextSelection -bool TRUE; killall Finder
 
 echo "\nDone. Moving on."
