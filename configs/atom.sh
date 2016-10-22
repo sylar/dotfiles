c165cdd490927b1e9fa1a23d9713ea368497746f
Andrei <andrei@constantinescu.io>
2016-10-22 13:12:03 +0100

    Update atom and npm packages.

---
+++ configs/atom.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo "Installing atom packages."
+apm install autocomplete-paths
+apm install base16-ocean-dark-spacegray
+apm install editorconfig
+apm install case-switch
+apm install sort-lines
+apm install toggle-quotes
+apm install file-icons
+apm install linter
+apm install linter-js-standard
+apm install standard-formatter
+apm install react
---
+++ configs/npm.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo "Installing npm packages."
+
+npm i -g node-gyp
+npm i -g ngrok
+npm i -g react-native-cli
+npm i -g serve
