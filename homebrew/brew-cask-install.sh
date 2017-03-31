afb5f48817b085df8408778016abb24a55c9c4ea
Andrei Constantinescu <andrei@constantinescu.io>
2017-03-31 16:54:30 +0100

    Remove flux. Add telegram.

--- homebrew/brew-cask-install.sh
+++ homebrew/brew-cask-install.sh
@@ -3,7 +3,6 @@ echo "Brewing GUI apps using \`brew cask\`."
 brew cask install 1password
 brew cask install alfred
 brew cask install atom
-brew cask install flux
 brew cask install google-chrome-canary
 brew cask install iterm2
 brew cask install messenger
@@ -14,5 +13,6 @@ brew cask install spotify
 brew cask install transmission
 brew cask install vlc
 brew casm install nosleep
+brew cask install telegram
 
 echo "Successfully brewed cask apps.\n"
