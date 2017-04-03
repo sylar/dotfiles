a0464a7d9885b7c8833ad4265bb8b1a482cff969
Andrei Constantinescu <andrei@constantinescu.io>
2017-04-03 23:36:08 +0100

    Fix typo.

--- homebrew/brew-install.sh
+++ homebrew/brew-install.sh
@@ -15,7 +15,7 @@ echo "Installing dev deps."
 brew install heroku-toolbelt
 brew install git-extras
 brew install node
-brew insatll yarn
+brew install yarn
 
 echo "Installing games and cool stuff."
 brew install archey
