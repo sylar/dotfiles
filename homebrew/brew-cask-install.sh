66acd99e6d8770c2da3beb2f854d36af12db74cb
Andrei Constantinescu <andrei@constantinescu.io>
2017-04-04 12:44:39 +0100

    Update canary path and add keybase, docker and captain.

--- homebrew/brew-cask-install.sh
+++ homebrew/brew-cask-install.sh
@@ -3,7 +3,7 @@ echo "Brewing GUI apps using \`brew cask\`."
 brew cask install 1password
 brew cask install alfred
 brew cask install atom
-brew cask install google-chrome-canary
+brew cask install Caskroom/versions/google-chrome-canary
 brew cask install iterm2
 brew cask install messenger
 brew cask install qlmarkdown
@@ -14,5 +14,8 @@ brew cask install transmission
 brew cask install vlc
 brew casm install nosleep
 brew cask install telegram
+brew cask install keybase
+brew cask install docker
+brew cask install captain
 
 echo "Successfully brewed cask apps.\n"
