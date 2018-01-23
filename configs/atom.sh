1baa7afbd1a4a82e5752f9d50ca774fe06943d2d
Andrei Constantinescu <andrei@constantinescu.io>
2018-01-23 21:31:41 +0000

    apm not apm-beta.

--- configs/atom.sh
+++ configs/atom.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
 echo "Installing atom packages."
-apm-beta install autocomplete-paths
-apm-beta install base16-ocean-dark-spacegray
-apm-beta install editorconfig
-apm-beta install case-switch
-apm-beta install sort-lines
-apm-beta install flow-ide
-apm-beta install toggle-quotes
-apm-beta install file-icons
-apm-beta install language-babel
-apm-beta install linter
-apm-beta install linter-eslint
-apm-beta install linter-js-standard
-apm-beta install linter-ui-default
-apm-beta install prettier-atom
-apm-beta install intentions
-apm-beta install busy-signal
-apm-beta install hyperclick
+apm install autocomplete-paths
+apm install base16-ocean-dark-spacegray
+apm install editorconfig
+apm install case-switch
+apm install sort-lines
+apm install flow-ide
+apm install toggle-quotes
+apm install file-icons
+apm install language-babel
+apm install linter
+apm install linter-eslint
+apm install linter-js-standard
+apm install linter-ui-default
+apm install prettier-atom
+apm install intentions
+apm install busy-signal
+apm install hyperclick
