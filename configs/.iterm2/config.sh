d0012595b0d1fd59fbec09d27d34414cf6bcba41
Andrei Constantinescu <andrei@constantinescu.io>
2018-12-05 10:16:03 +0000

    Add iterm2 configs.

---
+++ configs/.iterm2/config.sh
@@ -0,0 +1,4 @@
+
+curl -L https://iterm2.com/shell_integration/install_shell_integration_and_utilities.sh | bash
+cp -rv ./.iterm2_shell_integration.fish ~
+cp -rv ./.hushlogin ~
--- install.sh
+++ install.sh
@@ -14,6 +14,7 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 ./configs/atom.sh
 
+./configs/itemr2/config.sh
 cp .gitconfig ~
 cp .gitignore.global ~
 
