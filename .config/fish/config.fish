74f5537117d9613ab55ece125ff8b9bd62b29404
Andrei Constantinescu <andrei@constantinescu.io>
2018-12-05 10:18:31 +0000

    Update fish config.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,5 @@
 # Remove the newcomer fish greeting.
 set fish_greeting ""
 set -x GPG_TTY (tty)
+
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
