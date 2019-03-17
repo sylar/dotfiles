83ec9b2e6c507915e58605285fb5d5bf6b94c8ab
Andrei Constantinescu <andrei@constantinescu.io>
2019-03-17 20:24:50 +0000

    Grab tokens for Settings Sync from CLI.

--- scripts/vscode.sh
+++ scripts/vscode.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
+
 printf "%s\n\n" "Installing VS Code Settings Sync extension."
 code --install-extension shan.code-settings-sync
+
+# Grab the Github Token.
+read -p "Github token for `Settings Sync Extension` (or leave empty if non existant): " githubToken
+if [ githubToken ] then
+  jq '."sync.gist"'=\"$githubToken\" ./assets/.vscode/syncLocalSettings.tpl.json > ~/Library/Application\ Support/Code/User/syncLocalSettings.json
+fi
+
+# Grab Gist ID, or nothing.
+read -p "Github Gist ID (or leave empty if non existant): " gistId
+if [ gistId ] then
+  jq '."sync.gist"'=\"$gistId\" ./assets/.vscode/settings.tpl.json > ~/Library/Application\ Support/Code/User/settings.json
+fi
+
 printf "%s\n\n" "Installed VS Code Settings Sync extension."
-printf "%s" "Open VS Code and add the Github Token in order to sync your settings from the personal config gist."
