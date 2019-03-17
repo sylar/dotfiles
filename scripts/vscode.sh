76a4146dabe1d540207226cb372d1678bf5a05f5
Andrei Constantinescu <andrei@constantinescu.io>
2019-03-17 21:01:47 +0000

    Fix VS Code config script.

--- scripts/vscode.sh
+++ scripts/vscode.sh
@@ -4,14 +4,14 @@ printf "%s\n\n" "Installing VS Code Settings Sync extension."
 code --install-extension shan.code-settings-sync
 
 # Grab the Github Token.
-read -p "Github token for `Settings Sync Extension` (or leave empty if non existant): " githubToken
-if [ githubToken ] then
-  jq '."sync.gist"'=\"$githubToken\" ./assets/.vscode/syncLocalSettings.tpl.json > ~/Library/Application\ Support/Code/User/syncLocalSettings.json
+read -p "Github token for \"Settings Sync Extension\" (or leave empty if non existant): " githubToken
+if [ $githubToken ]; then
+  jq '."token"'=\"$githubToken\" ./assets/.vscode/syncLocalSettings.tpl.json > ~/Library/Application\ Support/Code/User/syncLocalSettings.json
 fi
 
 # Grab Gist ID, or nothing.
 read -p "Github Gist ID (or leave empty if non existant): " gistId
-if [ gistId ] then
+if [ $gistId ]; then
   jq '."sync.gist"'=\"$gistId\" ./assets/.vscode/settings.tpl.json > ~/Library/Application\ Support/Code/User/settings.json
 fi
 
