4282159a89d99a4d28ab9802eaad2b238d8765fc
Andrei Constantinescu <andrei@constantinescu.io>
2019-06-30 22:29:20 +0100

    Auto copy iterm2 config as well to ~.

--- scripts/iterm2.sh
+++ scripts/iterm2.sh
@@ -5,4 +5,8 @@ printf "%s\n\n" "Configuring iterm2 and fish shell."
 # Remove annoying shell last login message.
 cp -rv ./assets/.iterm2/.hushlogin ~
 
+# Copy iterm2 settings to the local system.
+mkdir ~/.config/iterm2
+cp -rv ./assets/.iterm2/iterm2pref.plist ~/.config/iterm2/com.googlecode.iterm2.plist
+
 printf "%s\n" "Configured iterm2 and fish shell."
