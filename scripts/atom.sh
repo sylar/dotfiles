b496fbf28e3b3595683e2ca892288754d5bd5517
Andrei Constantinescu <andrei@constantinescu.io>
2020-06-04 22:02:54 +0100

    Add settings sync to atom config.

--- scripts/atom.sh
+++ scripts/atom.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
 
-printf "%s\n\n" "Installing Atom packages."
+printf "%s\n\n" "Installing Atom Settings Sync extension."
+apm install sync-settings
 
-apm install atom-clock
-apm install atom-typescript
-apm install busy-signal
-apm install code-time
-apm install intentions
-apm install linter
-apm install linter-eslint
-apm install linter-ui-default
-apm install no-title-bar
-apm install prettier-atom
-apm install teletype
+# Grab the Github Token.
+read -p "Github token for \"Settings Sync Extension\" (or leave empty if non existant): " githubToken
+read -p "Github Gist ID (or leave empty if non existant): " gistId
 
-printf "%s\n\n" "Finished installing Atom packages."
+if [ $githubToken ] && [ $gistId ]; then
+  contents="$(jq --arg gistId "${gistId}" --arg githubToken "${githubToken}" '(."*"."sync-settings".gistId?) |= $gistId | (."*"."sync-settings".personalAccessToken?) |= $githubToken' ./lib/atom/config.tpl.json)"
+  echo $contents | json2cson --2spaces > ./assets/.atom/config.cson
+fi
+printf "%s\n\n" "Installing Atom Settings Sync extension."
