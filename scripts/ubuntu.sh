3168ce3c17c0b13546251a11d54a7b638a695654
Andrei Constantinescu <andrei@constantinescu.io>
2021-07-22 08:39:00 +0100

    Add brave browser to ubuntu script.

--- scripts/ubuntu.sh
+++ scripts/ubuntu.sh
@@ -30,3 +30,11 @@ kr pair
 sudo apt-get install fish
 chsh -s /usr/bin/fish
 cp -rv ./assets/.config ~
+
+# Install Brave Browser
+
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+sudo apt update
+sudo apt install brave-browser
