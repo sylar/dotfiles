08d65c511ed880c959682b222b369c053ebf43f2
Andrei Constantinescu <andrei@constantinescu.io>
2023-11-12 12:08:34 +0000

    Add --no-save to the brew fish fn. Not all installs should be public

--- fish/.config/fish/functions/brew.fish
+++ fish/.config/fish/functions/brew.fish
@@ -2,7 +2,11 @@ function brew
     switch "$argv[1]"
         case install reinstall remove tap untap
             command brew $argv
-            command brew bundle dump --global --force
+            
+            if [ "$argv[2]" != "--no-save" ]
+                command brew bundle dump --global --force
+            end
+            
         case '*'
             command brew $argv
     end
