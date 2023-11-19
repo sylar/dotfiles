4deba7df0e7e775fd3ce15a410b1956c72561f3f
Sylar <me@sylar.dev>
2023-11-19 16:30:34 +0000

    Fix brew install no-safe flag

--- fish/.config/fish/functions/brew.fish
+++ fish/.config/fish/functions/brew.fish
@@ -1,12 +1,17 @@
 function brew
-    switch "$argv[1]"
+    set -l cmd $argv[1]
+    set -l restOfCmd (string match -v -- '--no-save' $argv)
+
+    switch "$cmd"
         case install reinstall remove tap untap
-            command brew $argv
-            
-            if [ "$argv[2]" != "--no-save" ]
+            if contains -- '--no-save' $argv
+                # Execute without saving
+                command brew $restOfCmd
+            else
+                # Execute and save
+                command brew $argv
                 command brew bundle dump --global --force
             end
-            
         case '*'
             command brew $argv
     end
