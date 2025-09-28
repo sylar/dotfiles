2be287cf231f6543e9719ab2b5ae72a597319e67
Andrei Constantinescu <andrei@constantinescu.io>
2025-09-28 10:41:36 +0100

    Use mise instead of asdf

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,19 +4,4 @@ set -x GPG_TTY (tty)
 
 set -g fish_user_paths "/opt/homebrew/bin" $fish_user_paths
 set -g fish_user_paths "/opt/homebrew/sbin" $fish_user_paths
-
-# ASDF configuration code
-if test -z $ASDF_DATA_DIR
-    set _asdf_shims "$HOME/.asdf/shims"
-else
-    set _asdf_shims "$ASDF_DATA_DIR/shims"
-end
-
-# Do not use fish_add_path (added in Fish 3.2) because it
-# potentially changes the order of items in PATH
-if not contains $_asdf_shims $PATH
-    set -gx --prepend PATH $_asdf_shims
-end
-set --erase _asdf_shims
-
-set -gx PATH (npm config get prefix)/bin $PATH
+set -gx fish_user_paths (npm config get prefix)/bin $fish_user_paths
